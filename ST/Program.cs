5fedea9860dd471595fb54022b3db56b30dceafa
EMRE5 <EMRE5@EB-LAPTOP>
2022-03-22 16:04:37 +0300

    Add project files.

---
+++ ST/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using AsilStok;
+
+namespace ST
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stock elma = new Stock("elma");
+            elma.PriceChanced += OnPriceChanged;
+            elma.Price = 20.4M;
+            elma.Price = 50;
+            Console.ReadKey();
+
+        }
+
+        static void OnPriceChanged(object o, PriceChancedEventArgs e)
+        {
+            Console.WriteLine("eski fiyat:" + e.oldPrice);
+            Console.WriteLine("yeni fiyat:" + e.newPrice);
+        }
+    }
+ }
+   
